faded93bed957421100c9a3ce40e415ab2b4495f
Lutfi HC <lutfihc@gmail.com>
2022-03-22 13:40:44 +0100

    Finished all user settings

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'bootstrap-sass', '~> 3.3.7'
 gem 'bcrypt', '3.1.12'
 # Faker
 gem 'faker'
+# Pagination styles
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220318153337) do
+ActiveRecord::Schema.define(version: 20220322120858) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +24,7 @@ ActiveRecord::Schema.define(version: 20220318153337) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
